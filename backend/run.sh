e4a156d228ef2d102964251f54dfb6a0f5c01558
senthilkumar <senthilkumar.cit@gmail.com>
2021-08-24 22:44:37 +1000

    added docker

--- backend/run.sh
+++ backend/run.sh
@@ -3,5 +3,13 @@
 echo "build the project"
 mvn clean package
 
-echo "starting the applicaiton"
-java -jar target/demo-0.0.1.jar
+if [ -x "$(command -v docker)" ]; then
+    echo "docker is available!!"
+    echo "building docker image"
+    docker build -f Dockerfile -t demo-app .
+    docker run -p 9001:9001 demo-app
+else
+    echo "docker not installed! skipping..."
+    echo "starting the application"
+    java -jar target/demo-0.0.1.jar
+fi
